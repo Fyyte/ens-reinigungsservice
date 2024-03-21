4c741c2ffbd326ae061f797ad7c5edf6cff211a0
wael <wael@dinweldik.com>
2024-03-21 17:36:28 +0100

    update some subpages

---
+++ src/routes/environment/+page.svelte
@@ -0,0 +1,123 @@
+<script>
+	import picnic from '$lib/assets/picnic.png';
+	import trash from '$lib/assets/trash.png';
+</script>
+
+<section class="content">
+	<h1>Unser Engagement für den Umweltschutz</h1>
+	<h2>Verantwortungsvolle Reinigung für eine grünere Zukunft</h2>
+	<p>
+		Bei E.N.S Reinigungsservice e.U. ist uns bewusst, dass unsere Geschäftsaktivitäten Hand in Hand
+		mit der Verantwortung für die Umwelt gehen. Unser Ziel ist es, nicht nur saubere und hygienische
+		Räumlichkeiten zu schaffen, sondern auch unseren Teil zum Schutz unseres Planeten beizutragen.
+		Wir glauben fest daran, dass jeder Schritt hin zu mehr Nachhaltigkeit zählt.
+	</p>
+
+	<h2>Unsere umweltfreundlichen Praktiken:</h2>
+	<ul class="text-cards">
+		<li>
+			<p>
+				<strong>Einsatz umweltfreundlicher Reinigungsmittel</strong> Wir wählen unsere Reinigungsprodukte
+				sorgfältig aus und bevorzugen Lösungen, die sowohl effektiv als auch umweltverträglich sind.
+				Dies bedeutet den Verzicht auf harte Chemikalien zugunsten von Produkten mit natürlichen und
+				biologisch abbaubaren Inhaltsstoffen.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>Wassersparende Reinigungstechniken</strong> Wasser ist eine kostbare Ressource, und wir
+				setzen innovative Reinigungsmethoden ein, die den Wasserverbrauch minimieren. Durch effiziente
+				Techniken und Geräte streben wir danach, unseren ökologischen Fußabdruck zu reduzieren.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>Recycling und Abfallmanagement</strong> Wir legen großen Wert auf die Reduzierung von
+				Abfällen und fördern aktives Recycling sowohl intern als auch bei unseren Kunden. Unser Ziel
+				ist es, das Bewusstsein für die Wichtigkeit von Recycling zu schärfen und praktische Lösungen
+				für die Mülltrennung anzubieten.
+			</p>
+		</li>
+		<li>
+			<p>
+				<strong>Bildung und Sensibilisierung</strong> Ein wichtiger Teil unseres Engagements ist die
+				Aufklärung unseres Teams und unserer Kunden über Umweltschutzpraktiken. Durch regelmäßige Schulungen
+				und Informationsmaterialien fördern wir ein tiefgreifendes Verständnis für die Bedeutung nachhaltiger
+				Reinigung.
+			</p>
+		</li>
+	</ul>
+
+	<div>
+		<p>
+			Unser Engagement für den Umweltschutz ist ein wesentlicher Bestandteil unserer
+			Unternehmensphilosophie. Wir sind stolz darauf, in jedem Aspekt unserer Arbeit proaktiv zu
+			sein und kontinuierlich nach neuen Wegen zu suchen, um unseren Beitrag zum Schutz der Umwelt
+			zu leisten.
+		</p>
+		<p>
+			<strong
+				>Entdecken Sie, wie E.N.S Reinigungsservice e.U. den Unterschied macht – für saubere Räume
+				und eine saubere Umwelt.</strong
+			>
+		</p>
+	</div>
+	<div class="images">
+		<img src={trash} alt="recycling" />
+		<img src={picnic} alt="picnic" />
+	</div>
+</section>
+
+<style lang="postcss">
+	.images {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: var(--size-4);
+
+		& img {
+			border: 1px solid #ddd; /* Example border */
+			border-radius: var(--size-2);
+		}
+
+		& img:hover {
+			-webkit-transform: scale(1.01);
+			transform: scale(1.01);
+		}
+		@media screen and (max-width: 768px) {
+			grid-template-columns: 1fr;
+		}
+	}
+	strong {
+		color: var(--color-primary);
+	}
+	p {
+		margin-block: var(--size-12);
+	}
+
+	.text-cards {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr); /* Two columns by default */
+		gap: var(--size-4);
+		margin: 0;
+		padding: 0;
+		list-style: none;
+
+		@media screen and (max-width: 768px) {
+			grid-template-columns: 1fr; /* One column on small screens */
+		}
+	}
+
+	.text-cards li {
+		background: #f9f9f9; /* Example background color */
+		border: 1px solid #ddd; /* Example border */
+		padding: var(--size-4);
+		border-radius: var(--size-2);
+	}
+
+	.text-cards li strong {
+		display: block;
+		font-size: 1.2em; /* Larger font size for the title */
+		margin-bottom: var(--size-2); /* Space below the title */
+		color: var(--color-primary); /* Title color */
+	}
+</style>
---
+++ src/routes/jobs/+page.svelte
@@ -0,0 +1,85 @@
+<!-- <script>
+	import jobs from '$lib/assets/jobs.png';
+</script> -->
+
+<section class="content">
+	<h1>Werden Sie Teil unseres Teams</h1>
+	<p>
+		Bei E.N.S Reinigungsservice e.U. wächst unser Kundenstamm stetig, und um diesen wachsenden
+		Anforderungen gerecht zu werden, sind wir auf der Suche nach zuverlässigen und engagierten
+		Mitarbeitern. Uns ist es wichtig, dass unser Team nicht nur aus Fachkräften besteht, sondern
+		auch aus Menschen, die mit Leidenschaft bei der Sache sind.
+	</p>
+	<p>
+		Wir bieten eine faire Bezahlung, ein motivierendes Arbeitsumfeld und den direkten Kontakt zu
+		Kunden, die Ihre Arbeit zu schätzen wissen. Unsere Vision ist es, dass jeder Mitarbeiter mit
+		einem Lächeln zur Arbeit kommt und dieses auch am Ende des Tages noch trägt. Dafür setzen wir
+		auf ein Team, das nicht nur fachlich kompetent ist, sondern auch Wert auf ein gepflegtes
+		Erscheinungsbild legt.
+	</p>
+	<p>
+		Wenn Sie Erfahrung in der Reinigungsbranche haben und Teil eines Unternehmens werden möchten,
+		das Wert auf Qualität, Mitarbeiterzufriedenheit und Umweltschutz legt, dann freuen wir uns
+		darauf, Sie kennenzulernen. Einzelheiten zu offenen Positionen und dem Bewerbungsverfahren
+		besprechen wir gerne in einem persönlichen Gespräch.
+	</p>
+	<!-- 	<div class="images">
+		<img src={jobs} alt="Ein Bild, das den Geist von E.N.S Reinigungsservice e.U. einfängt" />
+		<img src={jobs} alt="Ein Bild, das den Geist von E.N.S Reinigungsservice e.U. einfängt" />
+	</div> -->
+	<p>Sind Sie bereit, mit einem Lächeln zu uns zu stoßen? Wir freuen uns auf Ihre Bewerbung!</p>
+</section>
+
+<style lang="postcss">
+	.images {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: var(--size-4);
+
+		& img {
+			border: 1px solid #ddd; /* Example border */
+			border-radius: var(--size-2);
+		}
+
+		& img:hover {
+			-webkit-transform: scale(1.01);
+			transform: scale(1.01);
+		}
+		@media screen and (max-width: 768px) {
+			grid-template-columns: 1fr;
+		}
+	}
+	strong {
+		color: var(--color-primary);
+	}
+	p {
+		margin-block: var(--size-12);
+	}
+
+	.text-cards {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr); /* Two columns by default */
+		gap: var(--size-4);
+		margin: 0;
+		padding: 0;
+		list-style: none;
+
+		@media screen and (max-width: 768px) {
+			grid-template-columns: 1fr; /* One column on small screens */
+		}
+	}
+
+	.text-cards li {
+		background: #f9f9f9; /* Example background color */
+		border: 1px solid #ddd; /* Example border */
+		padding: var(--size-4);
+		border-radius: var(--size-2);
+	}
+
+	.text-cards li strong {
+		display: block;
+		font-size: 1.2em; /* Larger font size for the title */
+		margin-bottom: var(--size-2); /* Space below the title */
+		color: var(--color-primary); /* Title color */
+	}
+</style>
