fa4d0f733543591446782d2c5fbce8aad516ba32
wael <wael@dinweldik.com>
2024-04-09 16:06:17 +0200

    better blog previews

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -23,24 +23,53 @@
 				{/if}
 			</div>
 		{/if}
-		<div class="footer">
-			<slot name="eyebrow" />
-		</div>
 	</a>
 </svelte:element>
 
 <style>
-	.img:hover {
-		-webkit-transform: scale(1.01);
-		transform: scale(1.01);
+	.background-image .img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		z-index: 1;
+		transition: transform 0.3s ease;
 	}
+
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: rgba(0, 0, 0, 0.5);
+		z-index: 2;
+	}
+
 	.title {
+		position: absolute;
+		width: 100%;
+		bottom: 10%;
+		left: 0;
 		text-align: center;
 		font-weight: bold;
-		text-decoration: underline;
+		z-index: 3;
+		color: var(--color-primary);
+		background-color: var(--color-gray-50);
 	}
+
 	a {
+		display: block;
+		height: 100%;
 		text-decoration: none;
 		color: inherit;
+		position: relative;
+	}
+
+	a:hover {
+		-webkit-transform: scale(1.01);
+		transform: scale(1.01);
 	}
 </style>
--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -20,7 +20,7 @@
 <style>
 	.container {
 		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
 		gap: 1rem;
 	}
 
